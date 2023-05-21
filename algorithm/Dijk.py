28cad60ed1828e1155a6bed10e77b945cd43b8fa
cchun3 <you@example.com>
2023-05-21 15:38:31 -0400

    restuctured and refactored and redesigned code

---
+++ algorithm/Dijk.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+from i_planner import PlanStatus
+from grid_status import GridState
+from time import time, sleep
+
+class Dijkstra(PlannerInterface):
+    def __init__(self, grid_map, grid_queue):
+        super().__init__(grid_map)
+        self._grid_map = grid_map
+        self._grid_queue = grid_queue
+        # The grid should be share between UI and planner for the
+        # spontaneous adding/canceling obstacles
+    
+    def plan(self):
+        '''
+        pseudo
+            set g() of src to 0
+            add src to the priority q
+            while q is not empty and current standpoint is not dest
+                set the current status to visited
+                get neighors of current grid
+                    update g() of the neighbors and set the predecessor
+                    put the neighbor into the q if it's unvisited
+                    what if it's already in the q? is it ok to have same grid in the q?
+                            is it possible to do this in parallel manner?
+                       P(g=5) ---> U
+                                   ^
+                                   |
+                                   P(g=2)
+        '''
+        super().plan()
+        # need to make sure src and dest are assigned
+        self._grid_queue.put(self._map_info.src())
+        self._grid_queue.put(self._map_info.dest())
+        src = self._map_info.src()
+        src.set_g(0)
+        self._priority_queue.put(src)
+        current_grid = None
+        while not (self._priority_queue.empty() or self._map_info.is_dest(current_grid)):
+            if not self.running():
+                break
+            current_grid = self._priority_queue.get()
+            if self._grid_map.get_grid_state(current_grid.id()) == GridState.VISITED:
+                continue
+            # update the grid
+            self._grid_map.set_grid_state(current_grid, GridState.VISITED)
+            self._grid_queue.put(current_grid)
+            # TODO: update the grid in the map
+            neighbors = self.get_neighbors(current_grid)
+            for nei in neighbors:
+                # nei might be already in the q, fastest way to update the g()?
+                if self._grid_map.get_grid_state(nei) != GridState.VISITED: # check if the grid is already visited in the map
+                    neighbor = self._grid_map.get_grid(nei[0], nei[1])
+                    neighbor.relax(current_grid)
+                    neighbor.set_state(GridState.SEEN)
+                    self._priority_queue.put(neighbor)
+                    self._grid_queue.put(neighbor)
+            sleep(0.05)
+            self.progress.emit()
+
+        # Done, get the path if reach dst
+        status = PlanStatus(self._map_info.is_dest(current_grid))
+        while status.success and current_grid != src:
+            current_grid.set_state(GridState.PATH)
+            status.add_node(current_grid)
+            current_grid = current_grid.pred()
+        self.finished.emit()
+        return status
+
+        
+        
--- base_class/grid_status.py
+++ base_class/grid_status.py
@@ -71,3 +71,73 @@ class Grid(QTableWidgetItem):
     
     def reset(self):
         self.__init__(self._row, self._col)
+
+@total_ordering
+class GridLFA(QTableWidgetItem):
+    GridColorMap = {GridState.SRC: Qt.green,
+                    GridState.DEST: Qt.red,
+                    GridState.UNVISITED: Qt.white,
+                    GridState.VISITED: Qt.gray,
+                    GridState.SEEN: Qt.yellow,
+                    GridState.OCCUPIED: Qt.black,
+                    GridState.PATH: Qt.blue}
+    def __init__(self, row, col, cost = math.inf):
+        super().__init__()
+        self._row = row
+        self._col = col
+        self._state = GridState.UNVISITED
+        self._g = cost
+        self._pred = None
+    
+    def k1(self):
+        pass
+
+    def k2(self):
+        pass
+    
+    def rhs(self, other):
+        '''
+        look ahead value of other.g + d(self, other)
+        '''
+        pass
+
+    def h(self):
+        return 0
+
+    def id(self):
+        return [self._row, self._col]
+
+    def __eq__(self, other):
+        return self.k1() == other.k1() and self.k2() == other.k2()
+    
+    def __lt__(self, other):
+        return self.k1() < other.k1() or self.k2() < other.k2()
+
+    def g(self):
+        return self._g
+    
+    def set_g(self, g):
+        self._g = g
+
+    def pred(self):
+        return self._pred
+    
+    def set_pred(self, pred):
+        self._pred = pred
+
+    def relax(self, potential_pred):
+        # set pred if relaxed
+        self.set_g(min(self.g(), potential_pred.g() + 1)) # TODO: this has to abstract, g + h
+        if self.g() == potential_pred.g() + 1:
+            self.set_pred(potential_pred)
+
+    def set_state(self, state):
+        self._state = state
+        self.setBackground(Grid.GridColorMap[self._state])
+
+    def state(self):
+        return self._state
+    
+    def reset(self):
+        self.__init__(self._row, self._col)
+
